ce26a803d27241692b75922f1b64a202fecbf313
faxitto <faxito@gmail.com>
2024-07-01 07:28:10 -0700

    faxito dunyanin en gozel qizi

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ let seher = prompt('oldugunuz3 seheri qeyd edin:')
 let teyinat = prompt('Getmek istediyiniz  seheri qeyd edin:')
 let suret = prompt('masinin  suretini qeyd edin:')
 let mesafe = prompt('Yolun mesafesini qeyd edin:')
-saat = mesafe/saat 
+let saat = mesafe/suret
 let result = 'Siz' + ' ' + seher + ' ' + 'seherinden' + ' ' + teyinat + ' ' +'seherine olan' + ' ' + mesafe + 'km mesafeni' + suret + 'km\saat liq masinla' + saat + ' saata catarsiz.'
 
 
